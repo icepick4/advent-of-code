245f6616795123d17e758e1631a72e3c1bb1aa2c
Icepick <remi.jara@laposte.net>
2022-12-04 13:48:33 +0100

    second day second part finished

---
+++ 2022/2/2-2.py
@@ -0,0 +1,45 @@
+
+def result(weapon1, weapon2):
+    if (weapon1 == 'A' and weapon2 == 'X') or (weapon1 == 'B' and weapon2 == 'Y') or (weapon1 == 'C' and weapon2 == 'Z'):
+        return 3 + weaponValue(weapon2)
+    elif (weapon1 == 'A' and weapon2 == 'Y') or (weapon1 == 'B' and weapon2 == 'Z') or (weapon1 == 'C' and weapon2 == 'X'):
+        return 6 + weaponValue(weapon2)
+    elif (weapon1 == 'A' and weapon2 == 'Z') or (weapon1 == 'B' and weapon2 == 'X') or (weapon1 == 'C' and weapon2 == 'Y'):
+        return 0 + weaponValue(weapon2)
+
+
+def weaponValue(weapon):
+    if weapon == 'X':
+        return 1
+    elif weapon == 'Y':
+        return 2
+    elif weapon == 'Z':
+        return 3
+
+
+def findWeaponAgainst(weapon, typeOfMatch):
+    if (weapon == 'A' and typeOfMatch == 'X') or (weapon == 'B' and typeOfMatch == 'Z') or (weapon == 'C' and typeOfMatch == 'Y'):
+        return 'Z'
+    elif (weapon == 'C' and typeOfMatch == 'X') or (weapon == 'A' and typeOfMatch == 'Z') or (weapon == 'B' and typeOfMatch == 'Y'):
+        return 'Y'
+    elif (weapon == 'B' and typeOfMatch == 'X') or (weapon == 'C' and typeOfMatch == 'Z') or (weapon == 'A' and typeOfMatch == 'Y'):
+        return 'X'
+
+
+def read_file(filename):
+    tab = []
+    with open(filename, 'r', encoding='utf-8') as f:
+        for line in f:
+            line = line.split(' ')
+            if line[1].endswith('\n'):
+                line[1] = line[1][:-1]
+            tab.append(line)
+    return tab
+
+
+if __name__ == '__main__':
+    rounds = read_file('input.txt')
+    SUM = 0
+    for round in rounds:
+        SUM += result(round[0], findWeaponAgainst(round[0], round[1]))
+    print(SUM)
